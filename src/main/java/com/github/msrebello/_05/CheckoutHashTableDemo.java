6e7ea6f7c7f9c15ba47dfa71227cd25c8dec00ca
Matheus <matheussrebellodev@gmail.com>
2025-05-28 01:15:40 -0300

    feat: implementa exemplo de mapeamento e consulta de preços de itens
    Este commit:
    - Adiciona uma demonstração de mapeamento de items/preços usando
      implementação HashTable

---
+++ src/main/java/com/github/msrebello/_05/CheckoutHashTableDemo.java
@@ -0,0 +1,30 @@
+package com.github.msrebello._05;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Hashtable;
+import java.util.Locale;
+import java.util.Map;
+
+public class CheckoutHashTableDemo {
+
+    public static void main(String[] args) {
+        Map<String, BigDecimal> itemsPrices = new Hashtable<>();
+
+        itemsPrices.put("Apple", new BigDecimal("4.50"));
+        itemsPrices.put("Avocado", new BigDecimal("6.00"));
+        itemsPrices.put("Lemon", new BigDecimal("1.20"));
+        itemsPrices.put("Orange", new BigDecimal("5.00"));
+
+        printItemsAndTheirPrices(itemsPrices);
+    }
+
+    private static void printItemsAndTheirPrices(Map<String, BigDecimal> itemsPrices) {
+        NumberFormat format = NumberFormat.getCurrencyInstance(Locale.US);
+        itemsPrices.forEach((item, price) -> {
+            String dollarPrice = format.format(price);
+            System.out.printf("Item: %s - Price: %s\n", item, dollarPrice);
+        });
+    }
+
+}
